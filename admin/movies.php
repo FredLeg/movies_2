06a019f3d25c4dd617481447beaa799afaa5f54b
- <->
2015-07-02 16:22:31 +0200

    insert_movies

--- admin/movies.php
+++ admin/movies.php
@@ -39,8 +39,51 @@ $errors = [];
 
 if (empty($_POST)) {
 	foreach($fields as $field_name => $field_params) {
-		echo $field_name.'<br/>';
+
+		if ($field_params['required'] !== false && empty($_POST[$field_name])) {
+
+			$error_label = !empty($field_params['error']) ? $field_params['error'] : $field_name.' is mandatory';
+
+			$errors[$field_name] = $error_label;
+		}
+}
+
+if (empty($errors)) {
+
+		$query = $db->prepare('INSERT INTO movies SET slug = :slug, title = :title, year = :year, genres = :genres, synopsis = :synopsis, directors = :directors, actors = :actors, writers = :writers, runtime = :runtime, mpaa = :mpaa, rating = :rating, popularity = :popularity, poster_flag = :poster_flag, modified = NOW(), created = NOW()');
+		$query->bindValue('slug', $slug);
+		$query->bindValue('title', $title);
+		$query->bindValue('year', $year);
+		$query->bindValue('genres', $genres);
+		$query->bindValue('synopsis', $synopsis);
+		$query->bindValue('directors', $directors);
+		$query->bindValue('actors', $actors);
+		$query->bindValue('writers', $writers);
+		$query->bindValue('runtime', $runtime);
+		$query->bindValue('mpaa', $mpaa);
+		$query->bindValue('rating', $rating);
+		$query->bindValue('popularity', $popularity);
+		$query->bindValue('poster_flag', $poster_flag);
+		$query->execute();
+
+		$result = $db->lastInsertId();
+
+		if (empty($result)) {
+			echo '<div class="alert alert-danger" role="danger">Une erreur est survenue</div>';
+		} else {
+			echo '<div class="alert alert-success" role="success">Le film a bien été ajouté</div>';
+			echo redirectJs('movies.php');
+		}
+		goto footer;
 	}
+}
+
+if (!empty($errors)) {
+	echo '<div class="alert alert-danger" role="danger">';
+	foreach($errors as $error) {
+		echo $error.'<br>';
+	}
+	echo '</div>';
 }
 
 ?>
@@ -48,17 +91,50 @@ if (empty($_POST)) {
 <form class="form-horizontal" action="" method="POST" novalidate>
 
 <?php
-foreach($fields as $field) {
+foreach($fields as $field_name => $field_params) {
 
-echo PHP_EOL;
-?>
-<div class="form-group">
-	<label for="<?= $field ?>" class="col-sm-2 control-label"><?= $field ?></label>
-	<div class="col-sm-3">
-		<input type="text" id="<?= $field ?>" name="<?= $field ?>" class="form-control" placeholder="<?= $field ?>" value="">
+	$required = $field_params['required'];
+	$type = $field_params['type'];
+	$maxlength = $field_params['maxlength'];
+	$label = ucfirst(!empty($field_params['label']) ? $field_params['label'] : $field_name);
+
+	echo PHP_EOL;
+
+	if ($type == 'textarea') {
+	?>
+	<div class="form-group">
+		<label for="<?= $field_name ?>" class="col-sm-2 control-label"><?= $label ?></label>
+		<div class="col-sm-6">
+			<textarea id="<?= $field_name ?>" name="<?= $field_name ?>" class="form-control" placeholder="<?= $label ?>" rows="5" style="resize: none;"><?= $$field_name ?></textarea>
+		</div>
 	</div>
-</div>
-<?php } ?>
+	<?php } else if ($type == 'checkbox') { ?>
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<div class="checkbox">
+				<label for="<?= $label ?>">
+					<input id="<?= $field_name ?>" type="checkbox" name="<?= $field_name ?>" value="1" <?= $$field_name ? 'checked' : '' ?>> <?= $label ?>
+				</label>
+			</div>
+		</div>
+	</div>
+	<?php } else { ?>
+	<div class="form-group">
+		<label for="<?= $field_name ?>" class="col-sm-2 control-label"><?= $label ?></label>
+		<div class="col-sm-6">
+			<input type="text" id="<?= $field_name ?>" name="<?= $field_name ?>" class="form-control" placeholder="<?= $label ?>" value="<?= $$field_name ?>">
+		</div>
+	</div>
+	<?php
+	}
+}
+?>
+
+	<div class="form-group">
+		<div class="col-sm-offset-2 col-sm-10">
+			<button type="submit" class="btn btn-default">Envoyer</button>
+		</div>
+	</div>
 
 </form>
 
